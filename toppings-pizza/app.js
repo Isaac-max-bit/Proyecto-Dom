a71b0efc741cba366e6f307a4d82636ea64cc7d4
Isaac Garcia <garciaalejoisaac@gmail.com>
2024-11-17 18:36:47 -0500

    Avance 17/11/2024

---
+++ toppings-pizza/app.js
@@ -0,0 +1,62 @@
+// const toppings = document.getElementsByClassName('topping')
+// console.log(toppings[3]);
+
+// const misToppings = document.getElementsByTagName('li');
+// console.log(misToppings);
+
+// const toppingsFondoMarron = document.getElementsByClassName('fondo-marron');
+// console.log(toppingsFondoMarron);
+
+// const aceitunas = document.querySelector('.topping');
+// console.log(typeof aceitunas);
+
+// const primerToppingNaranja = document.querySelector('ul li.fondo-naranja');
+// console.log(primerToppingNaranja);
+
+// const primerToppingNoMarron = document.querySelector('ul li:not(.fondo-marron)');
+// console.log(primerToppingNoMarron);
+
+// const toppingsNaranja = document.querySelectorAll('.topping.fondo-naranja');
+// console.log(toppingsNaranja);
+
+// const contenedor = document.getElementById('contenedor');
+// console.log(contenedor);
+
+
+// const contenedor = document.getElementById('contenedor');
+// console.log(contenedor.innerHTML);
+
+// const contenedor = document.getElementById('contenedor');
+// console.log(typeof contenedor.innerHTML);
+
+// const titulo = document.getElementById('titulo');
+// console.log(titulo);
+
+// const titulo = document.getElementById('titulo');
+// console.log(typeof titulo);
+
+// const titulo = document.getElementById('titulo');
+// console.log( typeof titulo.innerText);
+
+// const titulo = document.getElementById('titulo');
+// console.log(titulo.tagName);
+
+// const titulo = document.getElementById('titulo');
+// console.log(titulo);
+
+// const titulo = document.getElementById('tit');
+// console.log(titulo);
+
+// const primerTopping = document.querySelector('.topping');
+// console.log(primerTopping);
+
+// const primerTopping = document.querySelector('.topping');
+// console.log(primerTopping.style);
+
+const primerTopping = document.querySelector('.topping');
+
+// primerTopping.style.backgroundColor = 'blue';
+// primerTopping.style.color = '#6dff00';
+// primerTopping.style.textTransform = 'uppercase';
+
+console.log(primerTopping.style);
--- toppings-pizza/script.js
+++
@@ -1,2 +0,0 @@
-const titulo = document.getElementById('titulo');
-console.log(titulo);
